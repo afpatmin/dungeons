af8eebea8d7bfa4a9dcbf1834bc85a4ddf5f481a
Patrick Minogue <patrick.minogue@gmail.com>
2019-08-09 23:29:07 +0200

    now loading texture/sprite

--- src/states/game.rs
+++ src/states/game.rs
@@ -1,16 +1,75 @@
+pub const CAMERA_WIDTH: f32 = 100.0;
+pub const CAMERA_HEIGHT: f32 = 100.0;
+
 use amethyst::{
-    assets::{AssetStorage, Loader, Handle},
+    assets::{AssetStorage, Handle, Loader},
     core::transform::Transform,
-    ecs::prelude::{Component, DenseVecStorage},
     prelude::*,
     renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
 };
 
+use crate::components::Player;
+
 pub struct Game;
 
+impl Game {}
 
 impl SimpleState for Game {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+        init_camera(world);
+        let sprite_sheet = load_sprite_sheet(world);
+        init_player(world, sprite_sheet);
     }
+}
 
+fn init_player(world: &mut World, sprite_sheet: Handle<SpriteSheet>) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(CAMERA_WIDTH * 0.5, CAMERA_HEIGHT * 0.5, 0.0);
+    let sprite_render = SpriteRender {
+      sprite_sheet: sprite_sheet.clone(),
+      sprite_number: 0,
+    };
+
+    world.register::<Player>();
+    world
+        .create_entity()
+        .with(Player::new())
+        .with(sprite_render.clone())
+        .with(transform)
+        .build();
+}
+
+fn init_camera(world: &mut World) {
+    // (0, 0) is in the bottom left.
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(CAMERA_WIDTH * 0.5, CAMERA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(CAMERA_WIDTH, CAMERA_HEIGHT))
+        .with(transform)
+        .build();
+}
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "textures/pong_spritesheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "textures/pong_spritesheet.ron",
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    )
 }
